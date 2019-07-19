4fca3797feff8420c9836b26bdd5a2ed1f28ca9b
wanglei <rrwanglei@126.com>
2019-07-19 17:16:41 +0800

    project init

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+    <a href="hhttps://github.com/HappyPot/approval-ui">
+        <img width="200" src="https://github.com/HappyPot/approval-ui/blob/master/approval-ui.png">
+    </a>
+</p>
+eapproval-ui
+    <h3>A simple UI Toolkit built on Vue.js and iview.</h3>
+</h1>
+
+### Install approval-ui
+
+Using npm:
+```
+npm install approval-ui --save
+```
+
+You can find more info [on the website](https://www.iviewui.com/docs/guide/install-en).
+
+## Major Contributors
+
+| Name                                                 | Avatar                                                                                                                        |
+|------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------|
+| [HappyPot](https://github.com/HappyPot/eapproval-ui) | <img width="60" src="https://avatars0.githubusercontent.com/u/22255025?s=400&u=578b0fca8f1e72bb49632dc155ed597b90bdeed4&v=4"> |
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2019-present, approval-ui
+
+
+
---
+++ examples/components/HelloWorld.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="hello">
+    <!-- <ARemark v-model="text" ref="aremark" @on-blur="aa"></ARemark> -->
+    <ALog :columns="columns1" :data="approvalList"></ALog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      approvalList: [],
+      columns1: [
+        {
+          title: "节点名",
+          key: "ActivityName"
+        },
+        {
+          title: "用户名",
+          key: "UserName"
+        },
+        {
+          title: "操作",
+          key: "ActionCNName"
+        },
+        {
+          title: "操作日期",
+          key: "ActionDate"
+        },
+        {
+          title: "审批意见/备注",
+          key: "Comments"
+        }
+      ],
+      isError: false,
+      text: "", //mark的值
+      formValidate: {
+        name: "",
+        mail: "",
+        city: "",
+        gender: "",
+        interest: [],
+        date: "",
+        time: "",
+        desc: ""
+      },
+      ruleValidate: {
+        name: [
+          {
+            required: true,
+            message: "The name cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        mail: [
+          {
+            required: true,
+            message: "Mailbox cannot be empty",
+            trigger: "blur"
+          },
+          { type: "email", message: "Incorrect email format", trigger: "blur" }
+        ],
+        city: [
+          {
+            required: true,
+            message: "Please select the city",
+            trigger: "change"
+          }
+        ],
+        gender: [
+          { required: true, message: "Please select gender", trigger: "change" }
+        ],
+        interest: [
+          {
+            required: true,
+            type: "array",
+            min: 1,
+            message: "Choose at least one hobby",
+            trigger: "change"
+          },
+          {
+            type: "array",
+            max: 2,
+            message: "Choose two hobbies at best",
+            trigger: "change"
+          }
+        ],
+        date: [
+          {
+            required: true,
+            type: "date",
+            message: "Please select the date",
+            trigger: "change"
+          }
+        ],
+        time: [
+          {
+            required: true,
+            type: "string",
+            message: "Please select time",
+            trigger: "change"
+          }
+        ],
+        desc: [
+          {
+            required: true,
+            message: "Please enter a personal introduction",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 20,
+            message: "Introduce no less than 20 words",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    // aa(obj) {
+    //   // 判断是否为空值
+    //   !obj.val
+    //     ? (this.$refs.aremark.isError = true)
+    //     : (this.$refs.aremark.isError = false);
+    // }
+  }
+};
+</script>
+<style>
+.mm {
+  margin-top: 20px !important;
+}
+</style>
+
